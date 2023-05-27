a51465ab257651b4a93983ff3965fa7f39b861ac
Jardeson Santos <jardounifanor@gmail.com>
2023-05-27 13:45:38 -0300

    Adicionando Mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,17 @@ const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-pr
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild")
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', //for html reports
+  reporterOptions: {
+    reportPageTitle: 'Cypress BDD Report',
+    embeddedScreenshots: true,
+  },
+
   e2e: {
     specPattern: "cypress/e2e/features/*.feature",
     baseUrl: "http://automationpratice.com.br/",
     setupNodeEvents(on, config) {
+     
       // implement node event listeners here
       addCucumberPreprocessorPlugin(on, config);
       on(
@@ -16,6 +23,8 @@ module.exports = defineConfig({
           plugins: [createEsbuildPlugin(config)],
         })
       );
+      screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     },
   },
