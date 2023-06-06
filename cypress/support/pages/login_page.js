19c3ac40acdf61c25366f8c2d47c1d7cc1d8f950
Jardeson Santos <jardounifanor@gmail.com>
2023-06-06 19:53:42 -0300

    Add backspace vazio nos cenarios e refatorando pra function

--- cypress/support/pages/login_page.js
+++ cypress/support/pages/login_page.js
@@ -13,8 +13,8 @@ export default {
     cy.get(login_map.fields.password).type(password);
   },
   fillCredentials(email,password){
-    cy.get(login_map.fields.email).type(email, {force: true});
-    cy.get(login_map.fields.password).type(password,{force: true});
+    cy.get(login_map.fields.email).type(email);
+    cy.get(login_map.fields.password).type(password);
   },
   clicksLogin(){
     cy.get(login_map.buttons.btnLogin).click({force: true});
